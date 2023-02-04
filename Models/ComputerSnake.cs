b8391f0d8342fda33821b90d7221346e06ee43de
Casey Carroll <caseydean8@gmail.com>
2023-02-04 14:01:45 -0500

    Added model mapping functionality

--- Models/ComputerSnake.cs
+++ Models/ComputerSnake.cs
@@ -1,37 +1,27 @@
 // Add folder name "Models" to namespace.
 // "Computer" Model is decoupled from Program.cs and Main method.
-using System.Text.Json.Serialization;
-
 namespace ModelsTutorial.Models
 {
-    public class Computer
+    public class ComputerSnake
     {
-        [JsonPropertyName("computer_id")]
-        public int ComputerId { get; set; }
+        public int computer_id { get; set; }
         // private string _motherboard; private string Motherboard;
         // below acts as a property to the _motherboard field above
         // private string Motherboard { get { return _motherboard; } set { _motherboard = value; } }
         // Below has same functionality as the two above lines
-        [JsonPropertyName("motherboard")]
-        public string Motherboard { get; set; } = "";
+        public string motherboard { get; set; } = "";
 
-        [JsonPropertyName("cpu_cores")]
-        public int? CPUCores { get; set; } = 0;
+        public int? cpu_cores { get; set; } = 0;
 
-        [JsonPropertyName("has_wifi")]
-        public bool HasWifi { get; set; }
+        public bool has_wifi { get; set; }
 
-        [JsonPropertyName("has_lte")]
-        public bool HasLTE { get; set; }
+        public bool has_lte { get; set; }
 
-        [JsonPropertyName("release_date")]
-        public DateTime? ReleaseDate { get; set; }
+        public DateTime? release_date { get; set; }
 
-        [JsonPropertyName("price")]
-        public decimal Price { get; set; }
+        public decimal price { get; set; }
         // To fix non non-nullable warning, use = "";
-        [JsonPropertyName("video_card")]
-        public string VideoCard { get; set; } = "";
+        public string video_card { get; set; } = "";
 
         // Fix non-nullable string warning, see easier way above, this isn't necessary
         // public Computer()
